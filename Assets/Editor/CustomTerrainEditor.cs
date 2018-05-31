396fc99058b8d306cf0a9f822ba91d343ead2d3f
Unknown <velen_rendlich@hotmail.com>
2018-05-31 21:34:29 +0300

    Generating Procedural Height Maps

---
+++ Assets/Editor/CustomTerrainEditor.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using EditorGUITable; // We will get this from asset store
+
+[CustomEditor(typeof(CustomTerrain))] // Links the editor code in here with CustomTerrain class.
+[CanEditMultipleObjects]
+
+public class CustomTerrainEditor : Editor {
+    //properties ------
+    SerializedProperty randomHeightRange;
+    SerializedProperty heightMapScale;
+    SerializedProperty heightMapImage;
+
+    //fold outs ------
+    bool showRandom = false;
+    bool showLoadHeights = false;
+
+    private void OnEnable() {
+        
+        randomHeightRange = serializedObject.FindProperty("randomHeightRange"); // Linking the randomheightrange in the other script to this
+        heightMapScale = serializedObject.FindProperty("heightMapScale");
+        heightMapImage = serializedObject.FindProperty("heightMapImage");
+    }
+    public override void OnInspectorGUI() { // Graphical UI you see in the inspector for your custom terrain.
+        serializedObject.Update(); // Updates all of the serialized values going on between this script and customterrain
+        CustomTerrain terrain = (CustomTerrain) target; // Link to the CustomTerrain script, target is the object being inspected
+        showRandom = EditorGUILayout.Foldout(showRandom, "Random"); // Triangle in the inspector you use to open or close
+        if (showRandom) {
+            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+            GUILayout.Label("Set Heights Between Random Values", EditorStyles.boldLabel);
+            EditorGUILayout.PropertyField(randomHeightRange);
+            if (GUILayout.Button("Random Heights")) {
+                terrain.RandomTerrain();
+            }
+        }
+        showLoadHeights = EditorGUILayout.Foldout(showLoadHeights, "Load Heights");
+        if (showLoadHeights) {
+            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+            GUILayout.Label("Load Heights From Texture", EditorStyles.boldLabel);
+            EditorGUILayout.PropertyField(heightMapImage);
+            EditorGUILayout.PropertyField(heightMapScale);
+            if (GUILayout.Button("Load Texture")) {
+                terrain.LoadTexture();
+            }
+        }
+        EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+        if (GUILayout.Button("Reset Heights")) {
+            terrain.ResetTerrain();
+        }
+
+        serializedObject.ApplyModifiedProperties(); // Apply any changes that happened
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scene2/CustomTerrain.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Linq;
+    [ExecuteInEditMode]
+public class CustomTerrain : MonoBehaviour {
+
+    public Vector2 randomHeightRange = new Vector2(0, 0.1f);
+    public Texture2D heightMapImage;
+    public Vector3 heightMapScale = Vector3.one; 
+
+    public Terrain terrain;
+    public TerrainData terrainData;
+
+    public void RandomTerrain() {
+        float[,] heightMap = terrainData.GetHeights(0, 0, terrainData.heightmapWidth, terrainData.heightmapHeight);
+        for (int x = 0; x < terrainData.heightmapWidth; x++) {
+            for (int z = 0; z < terrainData.heightmapHeight; z++) {
+                heightMap[x, z] += Random.Range(randomHeightRange.x, randomHeightRange.y); // Adding instead of resetting
+            }
+        }
+        terrainData.SetHeights(0, 0, heightMap);
+    }
+    public void LoadTexture() {
+        float[,] heightMap = new float[terrainData.heightmapWidth, terrainData.heightmapHeight];
+        for (int x = 0; x < terrainData.heightmapWidth; x++) {
+            for (int z = 0; z < terrainData.heightmapHeight; z++) {
+                // Scaling in x, z our heightmap fits the resolution of the heightmapimage
+                // Scaling in y just multiplies the height
+                heightMap[x, z] = heightMapImage.GetPixel((int) (x * heightMapScale.x), (int) (z * heightMapScale.z)).grayscale * heightMapScale.y;  
+            }
+        }
+        terrainData.SetHeights(0, 0, heightMap);
+    }
+
+
+
+
+
+
+    public void ResetTerrain () {
+        float[,] heightMap = terrainData.GetHeights(0, 0, terrainData.heightmapWidth, terrainData.heightmapHeight);
+        for (int x = 0; x < terrainData.heightmapWidth; x++) {
+            for (int z = 0; z < terrainData.heightmapHeight; z++) {
+                heightMap[x, z] = 0; 
+            }
+        }
+        terrainData.SetHeights(0, 0, heightMap);
+    }
+
+    private void OnEnable() {  
+        Debug.Log("Initialising Terrain Data");
+        terrain = GetComponent<Terrain>();
+        terrainData = Terrain.activeTerrain.terrainData;
+    }
+    void Awake() {
+        SerializedObject tagManager = new SerializedObject(AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset")[0]); // Getting tag manager
+        SerializedProperty tagsProp = tagManager.FindProperty("tags"); // Getting tags property of tag manager
+    
+        AddTag(tagsProp, "Terrain");
+        AddTag(tagsProp, "Cloud");
+        AddTag(tagsProp, "Shore");
+
+        // Apply tag changes to tag database
+        tagManager.ApplyModifiedProperties();
+
+        // Take this object
+        gameObject.tag = "Terrain";
+
+    }
+    void AddTag(SerializedProperty tagsProp, string newTag) {
+        bool found = false;
+        // Ensure the tag does not already exist
+        for(int i = 0; i < tagsProp.arraySize; i++) {
+            SerializedProperty t = tagsProp.GetArrayElementAtIndex(i);
+            if(t.stringValue.Equals(newTag)) {
+                found = true;
+                break;
+            }
+        }
+        // Add your new tag
+        if(!found) {
+            tagsProp.InsertArrayElementAtIndex(0);
+            SerializedProperty newTagProp = tagsProp.GetArrayElementAtIndex(0);
+            newTagProp.stringValue = newTag;
+        }
+    }
+    private void Start() {
+        
+    }
+    private void Update() {
+    }
+}
